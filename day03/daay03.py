3d800f1d046ebc8d67d63d770ef222972ce2a13f
Corentin <corentin@restsolution.co>
2018-12-03 14:11:08 +0100

    Day 03 done

--- day03/daay03.py
+++ day03/daay03.py
@@ -1,3 +1,4 @@
+id = 'id'
 var = [
 {id: "#1",  "dim": [555, 891], "size": [18, 12]},
 {id: "#2",  "dim": [941, 233], "size": [16, 14]},
@@ -1313,14 +1314,26 @@ var = [
 tissue =  [['.' for x in range(1000)] for y in range(1000)]
 print(len(tissue[0]))
 
+hashed = {}
 for each in var:
-    for y in range(each['dim'][0], each['size'][0]):
-        for x in range(each['dim'][1], each['size'][1]):
+    for y in range(each['dim'][0], each['dim'][0] + each['size'][0]):
+        for x in range(each['dim'][1], each['dim'][1] + each['size'][1]):
+            hashed[each['id']] = {'required': each['size'][0] * each['size'][1], 'done': 0}
             if (tissue[y][x] == "."):
-                tissue[y][x] = each['id'][1:]
+                tissue[y][x] = each['id']
             else:
                 tissue[y][x] = '#'
 
 res = 0
+for y in range(1000):
+    for x in range(1000):
+        if (tissue[y][x] in hashed):
+            hashed[tissue[y][x]]['done'] += 1
+        if (tissue[y][x] == "#"):
+            res += 1
+
+for each in hashed:
+    if hashed[each]['required'] == hashed[each]['done']:
+        print(each)
 print(res)
  
